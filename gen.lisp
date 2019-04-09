08a2b6addabaf74dc236255db3abec4db1a5ff60
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-04-09 23:37:11 +0200

    indent

--- gen.lisp
+++ gen.lisp
@@ -14,47 +14,47 @@
   (defparameter *main-cpp-filename*
     (merge-pathnames "stage/cl-gen-cuda-try/source/cuda_try"
 		     (user-homedir-pathname)))
-  (let* ((code `(with-compilation-unit
-		    (include <stdio.h>)
-		  
-		 (raw "// https://www.youtube.com/watch?v=Ed_h2km0liI CUDACast #2 - Your First CUDA C Program")
-		 (raw "// https://github.com/NVIDIA-developer-blog/cudacasts/blob/master/ep2-first-cuda-c-program/kernel.cu")
-		 (function ("vector_add" ((a :type "int*")
-					  (b :type "int*")
-					  (c :type "int*")
-					  (n :type "int"))
-					 "__global__ void")
-			   
-			   (let ((i :type int :init threadIdx.x)
-				 )
-			     (if (< i n)
-				 (statements
-				  (setf (aref c i) (+ (aref a i)
-						      (aref b i)))))))
-		 (enum () (N 1024))
-		 (function ("main" ()
-				   "int")
-			   
-			   (let (,@(loop for e in '(a b c) collect
-					`(,e :type int* :init (funcall malloc (* N (funcall sizeof int)))))
-				 ,@(loop for e in '(a b c) collect
-					`(,(format nil "d_~a" e) :type int*))
-				   )
+  (let* ((code
+	  `(with-compilation-unit
+	       (include <stdio.h>)
+	     
+	     (raw "// https://www.youtube.com/watch?v=Ed_h2km0liI CUDACast #2 - Your First CUDA C Program")
+	     (raw "// https://github.com/NVIDIA-developer-blog/cudacasts/blob/master/ep2-first-cuda-c-program/kernel.cu")
+	     (function ("vector_add" ((a :type "int*")
+				      (b :type "int*")
+				      (c :type "int*")
+				      (n :type "int"))
+				     "__global__ void")
+		       
+		       (let ((i :type int :init threadIdx.x))
+			 (if (< i n)
+			     (statements
+			      (setf (aref c i) (+ (aref a i)
+						  (aref b i)))))))
+	     (enum () (N 1024))
+	     (function ("main" ()
+			       "int")
+		       
+		       (let (,@(loop for e in '(a b c) collect
+				    `(,e :type int* :init (funcall malloc (* N (funcall sizeof int)))))
 			     ,@(loop for e in '(a b c) collect
-				    `(funcall cudaMalloc (,(format nil "&d_~a" e) (* N (funcall sizeof int)))))
-			     (dotimes (i N)
-			       (setf (aref a i) i
-				     (aref b i) i
-				     (aref c i) 0))
-			     ,@(loop for e in '(a b c) collect
-				    `(funcall cudaMemcpy (,(format nil "d_~a" e) ,e (* N (funcall sizeof int))
-							   cudaMemcpyHostToDevice)))
-			     (funcall "vector_add<<<1,N>>>" d_a d_b d_c N)
-			     (funcall cudaMemcpy c d_c (* N (funcall sizeof int))
-				      cudaMemcpyDeviceToHost)
-			     ,@(loop for e in '(a b c) collect
-				    `(statements
-				      (funcall free ,e)
-				      (funcall cudaFree ,(format nil "d_~a" e))))
-			     (return 0))))))
+				    `(,(format nil "d_~a" e) :type int*))
+			       )
+			 ,@(loop for e in '(a b c) collect
+				`(funcall cudaMalloc (,(format nil "&d_~a" e) (* N (funcall sizeof int)))))
+			 (dotimes (i N)
+			   (setf (aref a i) i
+				 (aref b i) i
+				 (aref c i) 0))
+			 ,@(loop for e in '(a b c) collect
+				`(funcall cudaMemcpy (,(format nil "d_~a" e) ,e (* N (funcall sizeof int))
+						       cudaMemcpyHostToDevice)))
+			 (funcall "vector_add<<<1,N>>>" d_a d_b d_c N)
+			 (funcall cudaMemcpy c d_c (* N (funcall sizeof int))
+				  cudaMemcpyDeviceToHost)
+			 ,@(loop for e in '(a b c) collect
+				`(statements
+				  (funcall free ,e)
+				  (funcall cudaFree ,(format nil "d_~a" e))))
+			 (return 0))))))
     (write-source *main-cpp-filename* "cpp" code)))
