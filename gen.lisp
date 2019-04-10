ce9ef92c3446fb3472e7fe5739bb030c0c7086f2
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-04-10 09:41:50 +0200

    it works on vast.ai instance
    root@xxxxxxxx:~$ nvidia-smi
    Wed Apr 10 07:40:27 2019
    +-----------------------------------------------------------------------------+
    | NVIDIA-SMI 410.78       Driver Version: 410.78       CUDA Version: 10.0     |
    |-------------------------------+----------------------+----------------------+
    | GPU  Name        Persistence-M| Bus-Id        Disp.A | Volatile Uncorr. ECC |
    | Fan  Temp  Perf  Pwr:Usage/Cap|         Memory-Usage | GPU-Util  Compute M. |
    |===============================+======================+======================|
    |   0  GeForce GTX 107...  On   | 00000000:01:00.0 Off |                  N/A |
    |  0%   29C    P8    15W / 130W |      0MiB /  8119MiB |      0%      Default |
    +-------------------------------+----------------------+----------------------+
    
    +-----------------------------------------------------------------------------+
    | Processes:                                                       GPU Memory |
    |  GPU       PID   Type   Process name                             Usage      |
    |=============================================================================|
    |  No running processes found                                                 |
    +-----------------------------------------------------------------------------+
    root@xxxxxxxx:~$ ls
    onstart.log  onstart.sh
    root@xxxxxxxx:~$ ls
    cuda_try.cu  onstart.log  onstart.sh
    root@xxxxxxxx:~$ nvcc cuda_try.cu
    root@xxxxxxxx:~$ ls
    a.out  cuda_try.cu  onstart.log  onstart.sh
    root@xxxxxxxx:~$ ./a.out

--- gen.lisp
+++ gen.lisp
@@ -59,6 +59,6 @@
 				  (funcall cudaFree ,(format nil "d_~a" e))))
 			 (return 0))))))
     (write-source *main-cpp-filename* "cu" code)
-    (sb-ext:run-program "/usr/bin/scp" `("-C" ,(format nil "~a.cu" *main-cpp-filename*) "serv3:./"))))
+    (sb-ext:run-program "/usr/bin/scp" `("-C" ,(format nil "~a.cu" *main-cpp-filename*) "-l" "root" "serv3:./"))))
 
 
